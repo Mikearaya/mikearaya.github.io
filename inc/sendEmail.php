f203bdac4dcaa0cca97045681de059d71212a466
mikearaya <mikaelaraya12@gmail.com>
2020-01-30 15:08:28 +0300

    removed portfolio section

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'mikaelaraya12@gmail.com';
 
 
 if($_POST) {
@@ -65,5 +65,3 @@ if($_POST) {
 	} # end if - there was a validation error
 
 }
-
-?>
