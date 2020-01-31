3599c904925172587b24a2e7476ac59d4c152727
mikearaya <mikaelaraya12@gmail.com>
2020-01-31 17:17:33 +0300

    added mail header to phpMail

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -4,14 +4,14 @@
 $siteOwnersEmail = 'mikaelaraya12@gmail.com';
 
 
-if($_POST) {
+if ($_POST) {
 
-   $name = trim(stripslashes($_POST['contactName']));
-   $email = trim(stripslashes($_POST['contactEmail']));
-   $subject = trim(stripslashes($_POST['contactSubject']));
-   $contact_message = trim(stripslashes($_POST['contactMessage']));
+	$name = trim(stripslashes($_POST['contactName']));
+	$email = trim(stripslashes($_POST['contactEmail']));
+	$subject = trim(stripslashes($_POST['contactSubject']));
+	$contact_message = trim(stripslashes($_POST['contactMessage']));
 
-   // Check Name
+	// Check Name
 	if (strlen($name) < 2) {
 		$error['name'] = "Please enter your name.";
 	}
@@ -23,35 +23,40 @@ if($_POST) {
 	if (strlen($contact_message) < 15) {
 		$error['message'] = "Please enter your message. It should have at least 15 characters.";
 	}
-   // Subject
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+	// Subject
+	if ($subject == '') {
+		$subject = "Contact Form Submission";
+	}
 
 
-   // Set Message
-   $message .= "Email from: " . $name . "<br />";
+	// Set Message
+	$message .= "Email from: " . $name . "<br />";
 	$message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
-   $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+	$message .= "Message: <br />";
+	$message .= $contact_message;
+	$message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
 
-   // Set From: header
-   $from =  $name . " <" . $email . ">";
+	// Set From: header
+	$from =  $name . " <" . $email . ">";
 
-   // Email Headers
+	// Email Headers
 	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Reply-To: " . $email . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	$header .= "X-Priority: 1\r\n";
 
 
-   if (!$error) {
+	if (!$error) {
 
-      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
-      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+		ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+		$mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
-		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
-		
+		if ($mail) {
+			echo "OK";
+		} else {
+			echo "Something went wrong. Please try again.";
+		}
 	} # end if - no validation error
 
 	else {
@@ -59,9 +64,8 @@ if($_POST) {
 		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
 		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
 		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-		
-		echo $response;
 
+		echo $response;
 	} # end if - there was a validation error
 
 }
